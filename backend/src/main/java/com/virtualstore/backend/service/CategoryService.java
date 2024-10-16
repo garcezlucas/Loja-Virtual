bf2a70ae3d03d3ef559d39be9ff10214907f4eef
LucasGarcez <garcezlucas.silva@gmail.com>
2024-10-16 12:23:06 -0300

    feature - creating CRUD categories

---
+++ backend/src/main/java/com/virtualstore/backend/service/CategoryService.java
@@ -0,0 +1,39 @@
+package com.virtualstore.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.virtualstore.backend.entity.Category;
+import com.virtualstore.backend.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category create(Category category) {
+        category.setCreationDate(new Date());
+        Category newCategory = categoryRepository.saveAndFlush(category);
+        return newCategory;
+    }
+
+    public Category update(Category category) {
+        category.setUpdateDate(new Date());
+        Category updateCategory = categoryRepository.saveAndFlush(category);
+        return updateCategory;
+    }
+
+    public void remove(Long id) {
+        Category category = categoryRepository.findById(id).get();
+        categoryRepository.delete(category);
+    }
+
+}
