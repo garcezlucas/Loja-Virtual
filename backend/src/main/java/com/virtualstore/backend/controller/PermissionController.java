406470cf8ece365dfac382d148ce35044891fa73
LucasGarcez <garcezlucas.silva@gmail.com>
2024-10-17 13:19:21 -0300

    feature - creating CRUD for permissions

---
+++ backend/src/main/java/com/virtualstore/backend/controller/PermissionController.java
@@ -0,0 +1,46 @@
+package com.virtualstore.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.virtualstore.backend.entity.Permission;
+import com.virtualstore.backend.service.PermissionService;
+
+@RestController
+@RequestMapping("/api/permission")
+public class PermissionController {
+
+    @Autowired
+    private PermissionService permissionService;
+
+    @GetMapping("/")
+    public List<Permission> getAllPermissions() {
+        return permissionService.getAllPermissions();
+    }
+
+    @PostMapping("/")
+    public Permission createPermission(@RequestBody Permission permission) {
+        return permissionService.create(permission);
+    }
+
+    @PutMapping("/")
+    public Permission updatePermission(@RequestBody Permission permission) {
+        return permissionService.update(permission);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removePermission(@PathVariable("id") Long id) {
+        permissionService.remove(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ backend/src/main/java/com/virtualstore/backend/service/PermissionService.java
@@ -0,0 +1,39 @@
+package com.virtualstore.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.virtualstore.backend.entity.Permission;
+import com.virtualstore.backend.repository.PermissionRepository;
+
+@Service
+public class PermissionService {
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    public List<Permission> getAllPermissions() {
+        return permissionRepository.findAll();
+    }
+
+    public Permission create(Permission permission) {
+        permission.setCreationDate(new Date());
+        Permission newPermission = permissionRepository.saveAndFlush(permission);
+        return newPermission;
+    }
+
+    public Permission update(Permission permission) {
+        permission.setUpdateDate(new Date());
+        Permission updatePermission = permissionRepository.saveAndFlush(permission);
+        return updatePermission;
+    }
+
+    public void remove(Long id) {
+        Permission permission = permissionRepository.findById(id).get();
+        permissionRepository.delete(permission);
+
+    }
+}
