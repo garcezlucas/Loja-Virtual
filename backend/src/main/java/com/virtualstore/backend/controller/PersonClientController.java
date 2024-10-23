d06a6fce6102abeab8366d733303390f3dab16df
LucasGarcez <garcezlucas.silva@gmail.com>
2024-10-23 17:21:48 -0300

    fix: fix jwtUtil e controllers

--- backend/src/main/java/com/virtualstore/backend/controller/PersonClientController.java
+++ backend/src/main/java/com/virtualstore/backend/controller/PersonClientController.java
@@ -3,14 +3,12 @@ package com.virtualstore.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.virtualstore.backend.dto.PersonClientRequestDTO;
 import com.virtualstore.backend.entity.Person;
 import com.virtualstore.backend.service.PersonClientService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
---
+++ backend/src/main/java/com/virtualstore/backend/entity/RefreshToken.java
@@ -0,0 +1,62 @@
+package com.virtualstore.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "idPerson")
+    private Person person;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Date expiryDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+}
