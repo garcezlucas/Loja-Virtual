7ba61c83bec9b18554be55ac30b5f966c9e7855e
LucasGarcez <garcezlucas.silva@gmail.com>
2024-11-05 11:06:00 -0300

    feat: creating shopping cart entity

---
+++ backend/src/main/java/com/virtualstore/backend/entity/ProductShopCart.java
@@ -0,0 +1,47 @@
+package com.virtualstore.backend.entity;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "product_cart")
+@Data
+public class ProductShopCart {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idProduct")
+    @JsonIgnore
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "idCart")
+    private ShopCart cart;
+
+    private Double price;
+
+    private Double quantity;
+
+    private String observation;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+}
---
+++ backend/src/main/java/com/virtualstore/backend/entity/ShopCart.java
@@ -0,0 +1,40 @@
+package com.virtualstore.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "cart")
+@Data
+public class ShopCart {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date acquisitionDate;
+    
+    private String observation;
+
+    private String situation;
+
+    @ManyToOne
+    @JoinColumn(name = "idPerson")
+    private Person person;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+}
