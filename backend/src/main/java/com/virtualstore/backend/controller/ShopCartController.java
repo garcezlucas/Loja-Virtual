d524b028f8e8b188393c584902f55d45e5083e52
LucasGarcez <garcezlucas.silva@gmail.com>
2024-11-05 11:55:54 -0300

    feat: creating shopping cart controller

---
+++ backend/src/main/java/com/virtualstore/backend/controller/ShopCartController.java
@@ -0,0 +1,49 @@
+package com.virtualstore.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.virtualstore.backend.entity.Person;
+import com.virtualstore.backend.entity.Product;
+import com.virtualstore.backend.entity.ShopCart;
+import com.virtualstore.backend.service.PersonService;
+import com.virtualstore.backend.service.ShopCartService;
+
+@Controller
+@RequestMapping("/api/cart")
+public class ShopCartController {
+    
+     @Autowired
+    private ShopCartService shopCartService;
+
+    @GetMapping("/")
+    public List<ShopCart> getAllPersons() {
+        return shopCartService.getAllCarts();
+    }
+
+    @PostMapping("/")
+    public ShopCart createPerson(@RequestBody ShopCart shopCart, @RequestBody Product product, @RequestBody Double quantity) {
+        return shopCartService.create(shopCart, product, quantity);
+    }
+
+    @PutMapping("/")
+    public ShopCart updatePerson(@RequestBody ShopCart shopCart, @RequestBody Product product, @RequestBody Double quantity) {
+        return shopCartService.update(shopCart, product, quantity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removePerson(@PathVariable("id") Long id) {
+        shopCartService.remove(id);
+        return ResponseEntity.ok().build();
+    }
+}
